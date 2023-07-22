e96806d60f7fd94ebc7b825dc2e3895dd32f05a6
Most Often Delicate <mostoftendelicate@gmail.com>
2023-07-22 17:45:28 -0400

    Updated

--- assets/mod.js
+++ assets/mod.js
@@ -21,6 +21,18 @@ document.querySelector('.prev-btn').addEventListener('click', () => {
   slideCarousel('prev');
 });
 
+// footer
+let week_names = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+    'November', 'December'];
+let date = new Date();
+let weekname = week_names[date.getDay()];
+let monthname = month_names[date.getMonth()];
+
+const dateoutput = document.querySelector('#currentdate');
+
+dateoutput.textContent = "Updated: " + weekname + ", " + date.getDate() + " " + monthname + " " + date.getFullYear();
+
 // popup functions for all images
 function profilePopup() {
   document.getElementById("profile2").classList.toggle("active");
